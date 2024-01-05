3b059d69d7d7840a1125bfef133594872966487d
vaishukonnur123 <vaishukonnur@gmail.com>
2024-01-05 13:09:25 +0530

    table

--- payroll/src/Configurations/tables/Tabledata.js
+++ payroll/src/Configurations/tables/Tabledata.js
@@ -1,19 +1,60 @@
+// export const tableContent = [
+//     {
+//         name: "name",
+//         label: "Name",
+//         dataType: "string",
+//         cssClass: "datastyle",
+//         clmncss : "style1"
+//     },
+//     {
+//         name: "email",
+//         label: "Email",
+//         dataType: "string",
+//         cssClass: "datastyle",
+//         clmncss : "style1"
+//     },
+    
+    
+    
+//   ];
+
 export const tableContent = [
     {
-        name: "name",
-        label: "Name",
+        name: "EMPLOYEE_NAME",
+        label: "EMPLOYEE NAME",
         dataType: "string",
         cssClass: "datastyle",
         clmncss : "style1"
     },
     {
-        name: "email",
-        label: "Email",
+        name: "DESIGNATION",
+        label: "DESIGNATION",
         dataType: "string",
-        cssClass: "datastyle",
+        cssClass: "datastyle1",
+        clmncss : "style1"
+    },
+    {
+        name: "CTC",
+        label: "CTC",
+        dataType: "string",
+        cssClass: "datastyle1",
+        clmncss : "style1"
+    },
+    {
+        name: "JOINING_DATE",
+        label: "JOINING DATE",
+        dataType: "Date",
+        cssClass: "datastyle2",
+        clmncss : "style1"
+    },
+    {
+        name: "ICON",
+        label: " ",
+        dataType: "",
+        cssClass: "datastyle2",
         clmncss : "style1"
     },
     
-    
+   
     
   ];
