39ca0f2ab9841b8da533b499796e999365d53ac6
iramsaba-m <mirjankar.iramsaba@infokalash.com>
2024-01-05 19:56:42 +0530

    working db.json

--- payroll/src/Pages/Employee/EmployeeComponent.js
+++ payroll/src/Pages/Employee/EmployeeComponent.js
@@ -1,32 +1,32 @@
 import React, { useEffect, useState } from 'react'
 import Card from '../../Configurations/cardcomponent/Card'
 import Button from '../../Configurations/Buttoncomponent/Button'
-import {CardData, ButtonData, tableContent, USERS_API, data} from './EmployeeContent'
+import {CardData, ButtonData, tableContent, USERS_API} from './EmployeeContent'
 import TableComponent from '../../Configurations/tables/TableComponent'
 import {getApiUrl} from '../../Api/getAPI/GetAPI'
 import axios from 'axios'
 
 const EmployeeComponent = () => {
 
-  //   const [data, setData] = useState([])
+    const [data, setData] = useState([])
   // const [isLoading, setIsLoading] = useState(true)
 
-  // const fetchData = async () => {
-  //   try {
-  //     const response = await axios.get(getApiUrl(USERS_API));
-  //     console.log('API Response:', response.data);
+  const fetchData = async () => {
+    try {
+      const response = await axios.get("http://localhost:3000/data");
+      console.log('API Response:', response.data);
 
-  //     setData(response.data);
-  //   } catch (error) {
-  //     console.error(`Error fetching ${USERS_API} data:`, error);
-  //   }
-  // };
-
-  // useEffect(() => {
+      setData(response.data);
+    } catch (error) {
+      console.error(`Error fetching ${USERS_API} data:`, error);
+    }
+  };
+  
+  useEffect(() => {
     
-  //     fetchData();
+      fetchData();
    
-  // }, []);
+  }, []);
 
   // const data = [
   //   { EMPLOYEE_NAME: 'John Doe', DESIGNATION: 'CEO',CTC:'$10',JOINING_DATE:'10/7/2023' },
@@ -35,7 +35,7 @@ const EmployeeComponent = () => {
   //   {  EMPLOYEE_NAME: 'Bob Johnson', DESIGNATION: 'Marketing',CTC:'$10' ,JOINING_DATE:'10/7/2023' },
   //   {  EMPLOYEE_NAME: 'Bob Johnson hgghh', DESIGNATION: 'Human resource',CTC:'$10',JOINING_DATE:'10/7/2023'  },
   // ];
-    0
+    
   return (
     <div>
         <div className='card p-2'>
@@ -46,7 +46,7 @@ const EmployeeComponent = () => {
           <div className='text-left ml-4 font-lg font-bold'>
             Detailed report
           </div>
-          <div className='text-right'>
+          <div className='text-right p-1 mr-4'>
             <Button Configs={ButtonData} />
           </div>
         </div>
