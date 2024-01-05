b4d79beebdfe8f661de3492e04f7766b9c967f01
vaishukonnur123 <vaishukonnur@gmail.com>
2024-01-05 20:19:45 +0530

    table ,edit,checkbox

--- payroll/src/Configurations/tables/DynamicTable.js
+++ payroll/src/Configurations/tables/DynamicTable.js
@@ -1,29 +1,56 @@
-import React from 'react';
-
-import TableStyle from './TableStyle'
-// import { tableContent } from './Tabledata';
+// // components/DynamicTable.js
+import React, { useState } from 'react';
+import { MdOutlineEdit } from 'react-icons/md';
+import TableStyle from './TableStyle';
 
 function DynamicTable({ config, data }) {
+  const [selectedRows, setSelectedRows] = useState([]);
+
+  const handleCheckboxChange = (row) => {
+    // Implement your checkbox change logic here
+    if (selectedRows.includes(row)) {
+      setSelectedRows(selectedRows.filter((selectedRow) => selectedRow !== row));
+    } else {
+      setSelectedRows([...selectedRows, row]);
+    }
+  };
+
+  const handleEdit = (row) => {
+    // Implement your edit logic here
+    console.log('Edit clicked for row:', row);
+  };
+
   return (
-    <table className=' border-2 hover:border-blue-500' >
-      <thead className=''>
-        <tr >
+    <table className='border-2 hover:border-blue-500'>
+      <thead>
+        <tr>
+          <th></th> {/* Checkbox column */}
           {config.map((column) => (
             <th key={column.name} className={TableStyle[column.clmncss]}>
-              {column.label}               
+              {column.label}
             </th>
           ))}
+          <th></th> {/* Edit column */}
         </tr>
       </thead>
       <tbody>
         {data.map((row, rowIndex) => (
           <tr key={rowIndex}>
+            <td>
+              <input
+                type="checkbox"
+                onChange={() => handleCheckboxChange(row)}
+                checked={selectedRows.includes(row)}
+              />
+            </td>
             {config.map((column) => (
               <td key={column.name} className={TableStyle[column.cssClass]} style={{ textAlign: 'center' }}>
-              {column.dataType === 'number' ? parseFloat(row[column.name]).toFixed() : row[column.name]}
-            </td>
-            
+                {column.dataType === 'number' ? parseFloat(row[column.name]).toFixed() : row[column.name]}
+              </td>
             ))}
+            <td>
+              <MdOutlineEdit onClick={() => handleEdit(row)} />
+            </td>
           </tr>
         ))}
       </tbody>
