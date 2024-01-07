4346403f7555d8a21c92f72b0edcd361b563ae6f
vaishukonnur123 <vaishukonnur@gmail.com>
2024-01-07 12:45:41 +0530

    button updated--styles and added buttonlogic file

---
+++ payroll/src/Configurations/Buttoncomponent/Buttonlogic.js
@@ -0,0 +1,31 @@
+// components/ButtonLogic.js
+import React, { useState } from 'react';
+import ButtonConfig from './ButtonConfig';
+import EmpForm from '../empform/EmpForm';
+import ButtonData from './ButtonData';
+
+const ButtonLogic = () => {
+  const [showBody, setShowBody] = useState(false);
+
+  const handleButtonClick = (label) => {
+  
+    if (label === 'Add Employee') {
+      setShowBody(true);
+    }
+    // Add more conditions as needed
+  };
+
+  return (
+    <div className="flex">
+      {showBody ? (
+        <EmpForm />
+      ) : (
+        <ButtonConfig Config={ButtonData} onClick={handleButtonClick} />
+      )}
+    </div>
+  );
+};
+
+export default ButtonLogic;
+
+
