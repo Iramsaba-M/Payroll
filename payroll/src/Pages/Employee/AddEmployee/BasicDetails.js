b3c484a547b5db0aa986964a8dfe748489116bf9
vaishukonnur123 <vaishukonnur@gmail.com>
2024-01-09 14:09:38 +0530

    basic details form

--- payroll/src/Pages/Employee/AddEmployee/BasicDetails.js
+++ payroll/src/Pages/Employee/AddEmployee/BasicDetails.js
@@ -1,9 +1,16 @@
-import React from 'react'
+// BasicDetails.js
+import React from 'react';
+import Form from '../../../Components/form/Form';
+import formData1 from'../../../Components/form/formData1.json';
 
 const BasicDetails = () => {
+  
   return (
-    <div>BasicDetails</div>
-  )
-}
+    <div>
+      <h2>Basic Details</h2>
+      <Form configs={formData1} />
+    </div>
+  );
+};
 
 export default BasicDetails;
