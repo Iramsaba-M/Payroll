edb5e161cd961d97cf21b36f2334497f8cc11f54
iramsaba-m <mirjankar.iramsaba@infokalash.com>
2024-01-09 12:19:25 +0530

    add employee form added

--- payroll/src/Configurations/tables/TableStyle.js
+++ payroll/src/Configurations/tables/TableStyle.js
@@ -2,8 +2,8 @@ const TableStyle = {
     
   
 
-  style1: "   text-center  px-10 py-2 border-b-black text-gray-400 bg-gray-100 text-sm",
-  datastyle: "   border-b-zinc-50 text-center font-bold text-black-800 px-10  py-4 text-sm ",
+  style1: "text-center px-10 py-2 border-b-black text-gray-400 bg-gray-100 text-sm",
+  datastyle: "border-b-zinc-50 text-center font-bold text-black-800 px-10  py-4 text-sm ",
   style2: "   border-b text-center text-center text-black px-10  py-4 text-sm ",
   datastyle1: "    border-b-zinc-50 border-b text-center text-black px-10  py-4 text-sm ",
   datastyle2: "  border-b-zinc-50  border-b text-center text-gray-400 px-10  py-4 text-sm ",
