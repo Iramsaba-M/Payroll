e3fbeafd25dd6c918d55cb9c79a64c50e12b7be0
unknown <vikas.a@infokalash.com>
2024-01-23 16:17:20 +0530

    Bankdetails components

--- payroll/src/Pages/Employee/AddEmployee/BankDetails.js
+++ payroll/src/Pages/Employee/AddEmployee/BankDetails.js
@@ -1,8 +1,14 @@
 import React from 'react'
+import BankDetailForm from '../../../Components/form/BankDetailForm/BankDetailForm'
+import BankDetailData from '../../../Components/form/BankDetailForm/BankDetailData'
 
 const BankDetails = () => {
   return (
-    <div>BankDetails</div>
+    <div>
+      <div className='bankdeatils'>
+        <BankDetailForm configs={BankDetailData}  />
+      </div>
+    </div>
   )
 }
 
