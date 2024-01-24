7485e1e0c11957ea543004138d9ad560450b3a2e
unknown <vikas.a@infokalash.com>
2024-01-24 16:41:49 +0530

    Bank_details

--- payroll/src/Components/form/BankDetailForm/BankDetailData.js
+++ payroll/src/Components/form/BankDetailForm/BankDetailData.js
@@ -1,34 +1,38 @@
-const  BankDetailData =[
-    {
-        "label": "Bank Name",
-        "type": "options",
-        "options": [
-            { "label": "Bank 1", "value": "B1" },
-            { "label": "Bank2", "value": "B2" }
-        ],
-        "placeholder": "",
-        "textcss": "standard",
-        
-      },
-      {
-        "label": "IFSC Code",
-        "type": "text",
-        "placeholder": "Enter Here",
-        "textcss": "standard"
-      },
-      
+const BankDetailData = [
   {
-      "label": "Account Number ",
-      "type": "number",
-      "placeholder": "Enter Here",
-      "textcss": "standard"
+    "label": "Bank Name",
+    "name":"bank_name",
+    "type": "options",
+    "options": [
+      { "name": "Bank 1", "value": "B1" },
+      { "name": "Bank2", "value": "B2" }
+    ],
+    "placeholder": "",
+    "textcss": "standard",
+
   },
   {
-    "label": "Branch Code ",
+    "label": "IFSC Code",
+    "name":"ifsc_code",
     "type": "text",
     "placeholder": "Enter Here",
     "textcss": "standard"
-},
-  
+  },
+
+  {
+    "label": "Account Number ",
+    "name":"account_number",
+    "type": "number",
+    "placeholder": "Enter Here",
+    "textcss": "standard"
+  },
+  {
+    "label": "Branch Code ",
+    "name":"branch_code",
+    "type": "text",
+    "placeholder": "Enter Here",
+    "textcss": "standard"
+  },
+
 ]
 export default BankDetailData
