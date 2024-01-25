a6fec9042eafd637ad062b2a05c29e60bb5a0884
ShashankBaikadi1 <baikadi.shashank@infokalash.com>
2024-01-25 16:26:50 +0530

    Documents

--- payroll/src/Pages/Employee/AddEmployee/Documents.js
+++ payroll/src/Pages/Employee/AddEmployee/Documents.js
@@ -1,8 +1,12 @@
 import React from 'react'
-
+import DocumentsData from '../../../Components/form/DocumentsForm/DocumentsData'
+import DocumentsForm from '../../../Components/form/DocumentsForm/DocumentsForm'
 const Documents = () => {
   return (
-    <div>Documents</div>
+    <div>
+
+<DocumentsForm configs={DocumentsData} />
+    </div>
   )
 }
 
