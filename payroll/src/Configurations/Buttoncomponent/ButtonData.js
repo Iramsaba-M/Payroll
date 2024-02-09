037e557896951799d6102efcefcda47c4f591c07
iramsaba-m <mirjankar.iramsaba@infokalash.com>
2024-02-09 17:14:17 +0530

    Button data changes

--- payroll/src/Configurations/Buttoncomponent/ButtonData.js
+++ payroll/src/Configurations/Buttoncomponent/ButtonData.js
@@ -19,17 +19,12 @@ export const ButtonDataNew = [
 ];
 
 export const ButtonDataNew1 = [
-<<<<<<< HEAD
   { label: 'Generate CTC ',  style: 'buttonStylew' , type:"submit"},
   { label: 'Revise CTC', style: 'buttonStylew1' ,type:"Button" },
-=======
-  { label: 'Generate CTC ',  style: 'buttonStyle' , type:"submit"},
-  { label: 'Revise CTC', style: 'buttonStyle3' ,type:"Button" }, 
 ];
 
 
 export const ButtonDataAdd = [
   { label: '+ Add Another Document',  style: 'buttonStyle3' },
->>>>>>> 3d99a31bf1a1d370db6a0f28e1b33c6457940891
 ];
 
